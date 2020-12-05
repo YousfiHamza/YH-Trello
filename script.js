1791ead21983015fcdfac1c9d6bea7feea536744
Hamza <hamzayousfi2@gmail.com>
2020-12-05 21:05:36 +0100

    persesting the new tasks on localStorage - PATCH -

--- script.js
+++ script.js
@@ -98,22 +98,28 @@ function updateDOM() {
     createItemEl(onHoldList, 0, backlogItem, i);
   });
   // Run getSavedColumns only once, Update Local Storage
+  updatedOnLoad = true;
+  updateSavedColumns();
 }
 
 // Allow Arrays to reflect the drag and drop feature
 function rebuildArrays() {
+  backlogListArray = [];
   for (let i = 0; i < backlogList.children.length; i++) {
     backlogListArray.push(backlogList.children[i].textContent);
   }
 
+  progressListArray = [];
   for (let i = 0; i < progressList.children.length; i++) {
     progressListArray.push(progressList.children[i].textContent);
   }
 
+  completeListArray = [];
   for (let i = 0; i < completeList.children.length; i++) {
     completeListArray.push(completeList.children[i].textContent);
   }
 
+  onHoldListArray = [];
   for (let i = 0; i < onHoldList.children.length; i++) {
     onHoldListArray.push(onHoldList.children[i].textContent);
   }
@@ -123,7 +129,6 @@ function rebuildArrays() {
 // When items starts Dragging
 function drag(e) {
   draggedItem = e.target;
-  console.log("draggedItem: ", draggedItem);
 }
 
 // Column Allows for item to drop
