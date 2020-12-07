245abd2b22c94a6bf53ece157c75e9aebb03af69
Hamza <hamzayousfi2@gmail.com>
2020-12-07 11:39:21 +0100

    add items to the lists

--- script.js
+++ script.js
@@ -102,6 +102,30 @@ function updateDOM() {
   updateSavedColumns();
 }
 
+// Add to column List + reset the text box
+function addToColumn(column) {
+  const itemText = addItems[column].textContent;
+  const selectedArray = listArrays[column];
+  selectedArray.push(itemText);
+  addItems[column].textContent = "";
+  updateDOM();
+}
+
+// Show Add Item Input Box
+function showInputBox(column) {
+  addBtns[column].style.visibility = "hidden";
+  saveItemBtns[column].style.display = "flex";
+  addItemContainers[column].style.display = "flex";
+}
+
+// Hide Item Input Box
+function hideInputBox(column) {
+  addBtns[column].style.visibility = "visible";
+  saveItemBtns[column].style.display = "none";
+  addItemContainers[column].style.display = "none";
+  addToColumn(column);
+}
+
 // Allow Arrays to reflect the drag and drop feature
 function rebuildArrays() {
   backlogListArray = [];
